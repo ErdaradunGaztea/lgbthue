d4fca67c84aeffe5e35d50f3c79597fd4a4e48a6
Laura <erdaradun.gaztea+github@gmail.com>
2026-07-31 00:36:28 +0200

    Copy `show_pride()` with extra parameters

---
+++ R/show_pride.R
@@ -0,0 +1,24 @@
+#' List available palettes
+#'
+#' @description Shows the names of available LGBTQ palettes and their metadata.
+#'
+#' @return An `lgbtq_palette_list` with matching palettes.
+#'
+#' @examples
+#' show_pride()
+#'
+#' @export
+show_pride <- function(tags = NULL, sizes = NULL) {
+  # TODO: Add checkmate
+  valid <- names(.pride)
+
+  if (!is.null(tags)) {
+    valid <- intersect(valid, unlist(.pride_tag_dict[tags], use.names = FALSE))
+  }
+
+  if (!is.null(sizes)) {
+    valid <- intersect(valid, unlist(.pride_size_dict[as.character(sizes)], use.names = FALSE))
+  }
+
+  .pride[valid]
+}
