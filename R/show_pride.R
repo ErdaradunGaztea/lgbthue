e47ae38b3dc844384e62dd5c59885a1b865522b7
Laura <erdaradun.gaztea+github@gmail.com>
2026-07-31 18:05:12 +0200

    Fix docs

--- R/show_pride.R
+++ R/show_pride.R
@@ -2,10 +2,22 @@
 #'
 #' @description Shows the names of available LGBTQ palettes and their metadata.
 #'
+#' @param ... \cr
+#'  These dots are for future extensions and must be empty.
+#' @param tags `character()` \cr
+#'  Tags to filter by; currently available:
+#'  `"rainbow"`, `"sexuality"`, `"gender"`, `"subculture"`, `"a-spec"`, `"galactian"`.
+#' @param sizes `integer()` \cr
+#'  Palette sizes to require.
+#'
 #' @return An `lgbtq_palette_list` with matching palettes.
 #'
 #' @examples
+#' # By default, all palettes are shown
 #' show_pride()
+#'
+#' # To find rainbow palettes of sizes 6 and 8:
+#' show_pride(tags = "rainbow", sizes = c(6, 8))
 #'
 #' @export
 show_pride <- function(..., tags = NULL, sizes = NULL) {
