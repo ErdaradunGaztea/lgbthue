b11eb9359edc0c99a507041728a69ad2f49fe141
Laura <erdaradun.gaztea+github@gmail.com>
2026-08-01 16:51:06 +0200

    Include flag data with palettes

--- data-raw/pride.R
+++ data-raw/pride.R
@@ -3,191 +3,236 @@
   rainbow = list(
     colors = c("#E40303", "#FF8C00", "#FFED00", "#008026", "#24408E", "#732982"),
     aliases = c("pride"),
-    tags = c("rainbow")
+    tags = c("rainbow"),
+    flag = "lgbtq_palette_hflag"
   ),
   lesbian = list(
     colors = c("#D62900", "#FF9B55", "#FFFFFF", "#D461A6", "#A50062"),
     aliases = c("les", "gay_woman", "wlw"),
-    tags = c("sexuality")
+    tags = c("sexuality"),
+    flag = "lgbtq_palette_hflag"
   ),
   gay = list(
     colors = c("#078D70", "#98E8C1", "#FFFFFF", "#7BADE2", "#3D1A78"),
     aliases = c("gay_man", "mlm"),
-    tags = c("sexuality")
+    tags = c("sexuality"),
+    flag = "lgbtq_palette_hflag"
   ),
   bisexual = list(
     colors = c("#D00070", "#8C4799", "#0032A0"),
     aliases = c("bi"),
-    tags = c("sexuality")
+    tags = c("sexuality"),
+    flag = "lgbtq_palette_hflag_outer"
   ),
   transgender = list(
     colors = c("#55CDFC", "#F7A8B8", "#FFFFFF"),
     aliases = c("trans"),
-    tags = c("gender")
+    tags = c("gender"),
+    flag = "lgbtq_palette_hflag_symmetrical"
   ),
   asexual = list(
     colors = c("#000000", "#A4A4A4", "#FFFFFF", "#810081"),
     aliases = c("ace"),
-    tags = c("sexuality")
+    tags = c("sexuality"),
+    flag = "lgbtq_palette_hflag"
   ),
   intersex = list(
     colors = c("#FFD800", "#7902AA"),
-    aliases = c("inter")
+    aliases = c("inter"),
+    flag = "lgbtq_palette_flag_circle"
   ),
   nonbinary = list(
     colors = c("#FFF430", "#FFFFFF", "#9C59D1", "#000000"),
     aliases = c("enby", "nb"),
-    tags = c("gender")
+    tags = c("gender"),
+    flag = "lgbtq_palette_hflag"
   ),
   # Rainbow variants
   philadelphia = list(
     colors = c("#000000", "#784F17", "#D12229", "#F68A1E", "#FDE01A", "#007940", "#24408E", "#732982"),
-    tags = c("rainbow")
+    tags = c("rainbow"),
+    flag = "lgbtq_palette_hflag"
   ),
   progress = list(
     colors = c("#D12229", "#F68A1E", "#FDE01A", "#007940", "#24408E", "#732982",
                "#FFFFFF", "#FFAFC8", "#74D7EE", "#784F17", "#000000"),
-    tags = c("rainbow")
+    tags = c("rainbow"),
+    flag = "lgbtq_palette_chevron5"
   ),
   # A-spectrum
   aromantic = list(
     colors = c("#3DA542", "#A7D379", "#FFFFFF", "#A4A4A4", "#000000"),
-    tags = c("sexuality", "a-spec")
+    tags = c("sexuality", "a-spec"),
+    flag = "lgbtq_palette_hflag"
   ),
   acesthetic = list(
     colors = c("#000000", "#A4A4A4", "#FFFFFF", "#FD82AE"),
-    tags = c("sexuality", "a-spec")
+    tags = c("sexuality", "a-spec"),
+    flag = "lgbtq_palette_hflag"
   ),
   analterous = list(
     colors = c("#000000", "#A4A4A4", "#FFFFFF", "#75A0FF"),
-    tags = c("sexuality", "a-spec")
+    tags = c("sexuality", "a-spec"),
+    flag = "lgbtq_palette_hflag"
   ),
   asensual = list(
     colors = c("#000000", "#A4A4A4", "#FFFFFF", "#FFD698"),
-    tags = c("sexuality", "a-spec")
+    tags = c("sexuality", "a-spec"),
+    flag = "lgbtq_palette_hflag"
   ),
   ace_spectrum = list(
     colors = c("#151747", "#7557AD", "#D992B4", "#FFEDED"),
-    tags = c("sexuality", "a-spec")
+    tags = c("sexuality", "a-spec"),
+    flag = "lgbtq_palette_hflag"
   ),
   aro_spectrum = list(
     colors = c("#3FA053", "#96D07C", "#F3FCCF", "#176363", "#052027"),
-    tags = c("sexuality", "a-spec")
+    tags = c("sexuality", "a-spec"),
+    flag = "lgbtq_palette_hflag"
   ),
   aroace = list(
     colors = c("#E28C00", "#ECCD00", "#FFFFFF", "#62AEDC", "#203856"),
-    tags = c("sexuality", "a-spec")
+    tags = c("sexuality", "a-spec"),
+    flag = "lgbtq_palette_hflag"
   ),
   alloace = list(
     colors = c("#D50100", "#F85858", "#FFFFFF", "#81007F"),
-    tags = c("sexuality", "a-spec")
+    tags = c("sexuality", "a-spec"),
+    flag = "lgbtq_palette_hflag"
   ),
   aroallo = list(
     colors = c("#3DA542", "#A8D379", "#FFFFFF", "#FBE34B", "#E8BB00"),
-    tags = c("sexuality", "a-spec")
+    tags = c("sexuality", "a-spec"),
+    flag = "lgbtq_palette_hflag"
   ),
   # Other sexualities
   pansexual = list(
     colors = c("#FF1B8D", "#FFDA00", "#1BB3FF"),
     aliases = c("pan"),
-    tags = c("sexuality")
+    tags = c("sexuality"),
+    flag = "lgbtq_palette_hflag"
   ),
   androsexual = list(
     colors = c("#00CCFF", "#603524", "#B79ADD"),
-    tags = c("sexuality")
+    tags = c("sexuality"),
+    flag = "lgbtq_palette_hflag"
   ),
   gynesexual = list(
     colors = c("#F3A9B6", "#903F2A", "#5A953B"),
-    tags = c("sexuality")
+    tags = c("sexuality"),
+    flag = "lgbtq_palette_hflag"
   ),
   abrosexual = list(
     colors = c("#37CBBD", "#95E9B1", "#FFFFFF", "#FEA396", "#F35777"),
-    tags = c("sexuality")
+    tags = c("sexuality"),
+    flag = "lgbtq_palette_hflag"
   ),
   omnisexual = list(
     colors = c("#FFABD7", "#FF55BE", "#240045", "#675FFE", "#8DA5FE"),
     aliases = c("omni"),
-    tags = c("sexuality")
+    tags = c("sexuality"),
+    flag = "lgbtq_palette_hflag"
   ),
+  # TODO: Verify
   omniromantic = list(
     colors = c("#FFABD7", "#FF55BE", "#003B4B", "#5BD9BA", "#91FFC5"),
-    tags = c("sexuality")
+    tags = c("sexuality"),
+    flag = "lgbtq_palette_hflag"
   ),
   lesbian_7 = list(
     colors = c("#A60061", "#B95594", "#D162A8", "#FFFFFF", "#E5ADD1", "#C64D53", "#8C1801"),
-    tags = c("sexuality")
+    tags = c("sexuality"),
+    flag = "lgbtq_palette_hflag"
   ),
   gay_7 = list(
     colors = c("#078D70", "#26CEAA", "#98E8C1", "#FFFFFF", "#7BADE2", "#5049CC", "#3D1A78"),
-    tags = c("sexuality")
+    tags = c("sexuality"),
+    flag = "lgbtq_palette_hflag"
   ),
   # Fem-masc genders
   demiboy = list(
     colors = c("#7F7F7F", "#C4C4C4", "#9DD7EA", "#FFFFFF"),
-    tags = c("gender")
+    tags = c("gender"),
+    flag = "lgbtq_palette_hflag_symmetrical"
   ),
   demigirl = list(
     colors = c("#7F7F7F", "#C4C4C4", "#FDADC8", "#FFFFFF"),
-    tags = c("gender")
+    tags = c("gender"),
+    flag = "lgbtq_palette_hflag_symmetrical"
   ),
   # Galactian alignment system
+  # TODO: Handle symbols on galactian flags
   helian = list(
     colors = c("#FF7367", "#BC3028", "#FFF7B8", "#000000", "#B5B5B5"),
-    tags = c("gender", "galactian")
+    tags = c("gender", "galactian"),
+    flag = "lgbtq_palette_hflag"
   ),
   lunarian = list(
     colors = c("#C6A5D0", "#85618D", "#FFF7B8", "#000000", "#B5B5B5"),
-    tags = c("gender", "galactian")
+    tags = c("gender", "galactian"),
+    flag = "lgbtq_palette_hflag"
   ),
   solarian = list(
     colors = c("#FFB187", "#FF7D51", "#FFF7B8", "#000000", "#B5B5B5"),
-    tags = c("gender", "galactian")
+    tags = c("gender", "galactian"),
+    flag = "lgbtq_palette_hflag"
   ),
   stellarian = list(
     colors = c("#B6D6C1", "#79C49D", "#FFF7B8", "#000000", "#B5B5B5"),
-    tags = c("gender", "galactian")
+    tags = c("gender", "galactian"),
+    flag = "lgbtq_palette_hflag"
   ),
   # Other genders
   genderfluid = list(
     colors = c("#FF76A4", "#FFFFFF", "#C011D7", "#000000", "#2F3CBE"),
-    tags = c("gender")
+    tags = c("gender"),
+    flag = "lgbtq_palette_hflag"
   ),
   genderqueer = list(
     colors = c("#B57EDC", "#FFFFFF", "#4A8123"),
-    tags = c("gender")
+    tags = c("gender"),
+    flag = "lgbtq_palette_hflag"
   ),
   agender = list(
     colors = c("#000000", "#BCC4C7", "#FFFFFF", "#B7F684"),
-    tags = c("gender")
+    tags = c("gender"),
+    flag = "lgbtq_palette_hflag_symmetrical"
   ),
   bigender = list(
     colors = c("#C479A2", "#EDA5CD", "#D6C7E8", "#FFFFFF", "#9AC7E8", "#6D82D1"),
-    tags = c("gender")
+    tags = c("gender"),
+    flag = "lgbtq_palette_hflag"
   ),
   # Subcultures
   amatopunk = list(
     colors = c("#531768", "#B74D71", "#DB966D", "#FFE7AA", "#4E8C69"),
-    tags = c("subculture")
+    tags = c("subculture"),
+    flag = "lgbtq_palette_hflag"
   ),
   bear = list(
     colors = c("#623804", "#D56300", "#FEDD63", "#FEE6B8", "#FFFFFF", "#555555", "#000000"),
-    tags = c("subculture")
+    tags = c("subculture"),
+    flag = "lgbtq_palette_hflag"
   ),
   butch = list(
     colors = c("#324070", "#6A7AA4", "#8A92A9", "#EDECEB", "#C0B4DB", "#764EC5", "#51048A"),
-    tags = c("subculture")
+    tags = c("subculture"),
+    flag = "lgbtq_palette_hflag"
   ),
   femme = list(
     colors = c("#7D0037", "#DE42F1", "#FDFDFD", "#F29C41", "#9E470E"),
-    tags = c("subculture")
+    tags = c("subculture"),
+    flag = "lgbtq_palette_hflag"
   ),
   otter = list(
     colors = c("#23A8FA", "#9AD7FC", "#FFFFFF", "#B4B4B4", "#2A2A2A"),
-    tags = c("subculture")
+    tags = c("subculture"),
+    flag = "lgbtq_palette_hflag"
   ),
   # Uncategorized
   queerhet = list(
-    colors = c("#4E8492", "#6CCB99", "#727D6E", "#DAC686", "#B86962")
+    colors = c("#4E8492", "#6CCB99", "#727D6E", "#DAC686", "#B86962"),
+    flag = "lgbtq_palette_hflag"
   ),
   questioning = list(
     colors = c("#FF8636", "#F9E134", "#36E32D", "#2499F5")
@@ -231,10 +276,9 @@ tags <- purrr::map(.pride, \(palette) {
     structure(
       palette[["colors"]],
       name = name,
-      # TODO: Are aliases and tags even necessary at this point?
       aliases = palette[["aliases"]],
       tags = palette[["tags"]],
-      class = "lgbtq_palette"
+      class = c(palette[["flag"]], "lgbtq_palette")
     )
   }) |>
   structure(class = "lgbtq_palette_list")
